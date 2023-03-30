436da016d040edea3964974ff9d2cc5734730754
JulioVianaDev <JulioVianaDev@gmail.com>
2023-03-30 15:21:22 -0300

    criando os posts ok

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-function handleSubmit(event){
+const form = document.getElementById("my-form");
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
   event.preventDefault();
-  console.log('opa')
+  const data = new FormData();
+
+  data.append("post[title]", event.target.title.value);
+  data.append("post[image]", event.target.image.files[0]);
+  submitToAPI(data);
+}
+
+function submitToAPI(data) {
+  fetch("http://localhost:3000/posts", {
+    method: "POST",
+    body: data,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const latestPost = document.getElementById("latest-post");
+      latestPost.innerHTML = `<img src="${data.image_url}" alt="Última imagem enviada">`;
+    })
+    .catch((error) => console.error(error));
 }
